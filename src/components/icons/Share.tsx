e2b75e5a2e4fed2c896d31a27eaa3a27160cdf20
pixeledcode <ashoaib540918@gmail.com>
2024-06-09 22:30:10 +0530

    fix share icon

--- src/components/icons/Share.tsx
+++ src/components/icons/Share.tsx
@@ -7,13 +7,13 @@ export const Share = ({ ...props }: React.SVGProps<SVGSVGElement>) => {
 			{...props}
 		>
 			<path
-				fill="var(--theme-icon)"
+				fill="var(--theme-icon-bg)"
 				fillRule="evenodd"
 				d="M38.683 10.658a3 3 0 0 1-1.341 4.025L18.708 24l18.634 9.317a3 3 0 0 1-2.684 5.366l-24-12a3 3 0 0 1 0-5.366l24-12a3 3 0 0 1 4.025 1.341Z"
 				clipRule="evenodd"
 			/>
 			<path
-				fill="var(--theme-icon-bg)"
+				fill="var(--theme-icon)"
 				d="M11 14.5a9.5 9.5 0 1 0 0 19 9.5 9.5 0 0 0 0-19ZM37 27.5a9.5 9.5 0 1 0 0 19 9.5 9.5 0 0 0 0-19ZM37 1.5a9.5 9.5 0 1 0 0 19 9.5 9.5 0 0 0 0-19Z"
 			/>
 		</svg>
