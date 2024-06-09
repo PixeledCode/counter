2adffb66f59bdd1c151833def9fe36cd96aa01b9
pixeledcode <ashoaib540918@gmail.com>
2024-06-09 13:08:25 +0530

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,50 @@
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			workbox: {
+				globPatterns: ['**/*'],
+				cleanupOutdatedCaches: false,
+			},
+			includeAssets: ['**/*'],
+			manifest: {
+				theme_color: '#43677f',
+				background_color: '#43677f',
+				display: 'standalone',
+				scope: '/',
+				start_url: '/index.html',
+				short_name: 'Counter',
+				description:
+					'A basic counter app which ability to add new counters, update or delete them',
+				name: 'Counter App',
+				icons: [
+					{
+						src: '/icon-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: '/icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: '/maskable_icon.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
