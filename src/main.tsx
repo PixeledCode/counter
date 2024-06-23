e0063be674cfe894678fbef0248ad8d527b25717
pixeledcode <ashoaib540918@gmail.com>
2024-06-23 18:47:46 +0530

    remove cross from toast

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import { Toaster } from '@/components/ui/sonner'
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<>
 		<App />
-		<Toaster richColors={true} closeButton />
+		<Toaster richColors={true} />
 	</>
 )
